300632d06e441f8bea6f12d9d2b07c8947a9b927
Super <minchao.dong@melot.cn>
2018-06-20 11:04:44 +0800

    feature: add Resources

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
+import VueLazyload from 'vue-lazyload'
+
+
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource)
+Vue.use(VueLazyload, {
+    error: require('./assets/loading.svg'),
+    loading: require('./assets/loading.svg'),
+    attempt: 1
+  }
+)
 
 /* eslint-disable no-new */
 new Vue({
